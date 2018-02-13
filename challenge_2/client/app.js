9d2c6391f331d6d7edf302c4ddec261688803541
David Seidenberg <davidseidenberg@gmail.com>
2018-02-13 10:36:15 -0800

    Make HTML skeleton

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,7 @@
+// client app should be able to submit JSON data to the server and display the resulting CSV response on the page
 
+// basic reqs: JSON data entry on client should be done using HTML form with single textarea input field
+// also need a submit button which sends the data via POST to the server
+
+// prevent page reloads via submit button
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+// serve up an index.html file and associateda assets via express
+// recieve JSON data from client app, and send back CSV formatted response
+// implement report generation logic on the server without external libraries
 
+// create a package.json to store project dependencies
 
+// assume the JSON data is regular. 
+// flatten and map the JSON into a single line of CSV
+  // assume child properties in JSON will be in property called children, assume no other props
+   // any props besides children must be mapped to a column in CSV
+
+// 1. Make Basic HTML page
+// 2. Set up package.json
+// 3. Implement basic Express Server and get them to communicate
