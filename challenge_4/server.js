206a8a2476141ce575ad181d60bab0063e123a6b
David Seidenberg <davidseidenberg@gmail.com>
2018-02-15 10:44:18 -0800

    Start up express server, send back index.html

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} request received to ${req.url}`);
+  next();
+});
+
+app.use(express.static('client'));
+
+
+
+
+
+app.listen(3000, () => {console.log('App is listening to port 3000...')})
 
